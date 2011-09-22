9cceaa1be80ec527222b11338cefc93c1b305957
liz <shengyan1985@gmail.com>
2011-09-22 21:21:40 +0800

    add pagenation

---
+++ pagenation/pagenation.js
@@ -0,0 +1,232 @@
+/**
+ * @fileoverview Pagenation
+ * @desc 分页组件
+ * @author 乔花<shengyan1985@gmail.com>
+ */
+ 
+
+KISSY.add('gallery/pagenation', function(S, undefined) {
+
+    var D = S.DOM, E = S.Event, doc = document;
+	
+	//定义变量和常量
+
+    /**
+	 * 构造器
+	 * @param {Object} cfg 配置参数
+	 * @return
+	 */
+    function Pagenation(cfg) {
+        Pagenation.superclass.constructor.apply(this, arguments);
+        this._init();
+    }
+	
+	//配置项
+    Pagenation.ATTRS = {
+        /**
+         * 分页的 DOM 容器
+         * @type String|HTMLElement|KISSY.Node
+         */
+        container: {
+            setter: function(v) {
+                if (S.isString(v)) {
+                    return S.one(v);
+                }
+                if (v.offset) return v;
+                return new S.Node(v);
+            }
+        },
+        /**
+         * 样式前缀
+         * @type String
+         */
+        cls: {
+            value: 'pg-'
+        },
+        /**
+         * 点击分页项时, 调用的函数
+         * @type Function
+         * 带三个参数:
+         *  - idx: 新页号
+         *  - pg obj: 当前分页对象
+         *  - ready: fn 供外部调用者, 当切换好页时, 更新分页HTML等后续操作
+         * note: 当 callback 返回 false 时, 停止事件冒泡
+         */
+        callback: {
+            value: function(idx, pg, ready) {
+                ready(idx);
+                return false;
+            }
+        },
+        /**
+         * 当前页序号
+         * @type Number 从0开始计
+         */
+        current_page: {
+            value: 0
+        },
+        /**
+         * 点击页号时, url 变化, 暂时不实现
+         */
+        link_to: {
+            value: '#'
+        },
+        /**
+         * 显示多少页.
+         * - 取奇数
+         * - 当为0时, 表示只显示上一页/下一页
+         * @type Number
+         */
+        max_display_page_count: {
+            value: 5
+        },
+        /**
+         * 总共多少页, 可不设
+         * @type Number
+         */
+        total_page: {
+        },
+        /**
+         * 首页序号从哪边开始计
+         * @type Number
+         */
+        first_page: {
+            value: 0
+        },
+        /**
+         * 下一页文案
+         * @type String
+         */
+        next_text: {
+            value: '下一页'
+        },
+        /**
+         * 是否有下一页, 供那些不明确页数情况下使用
+         * @type Boolean
+         */
+        has_next: {
+            value: true
+        },
+        /**
+         * 上一页文案
+         * @type String
+         */
+        prev_text: {
+            value: '上一页'
+        },
+        /**
+         * 是否省略多页
+         * @type Boolean
+         */
+        ellipse_text: {
+            value: true
+        },
+        /**
+         * 是否初始加载第一页
+         * @type Boolean
+         */
+        load_first_page: {
+            value: true
+        }
+    };
+    S.extend(Pagenation, S.Base, {
+        _init: function() {
+            var self = this;
+
+            self.update();
+            self._bind();
+
+            // 载入第一页
+            if (self.get('load_first_page')) {
+                self.page(self.get('first_page'));
+            }
+        },
+        /**
+         * 根据当前状态, 构建HTML
+         */
+        update: function() {
+            var self = this,
+                current_page = self.get('current_page'),
+                cls = self.get('cls'),
+                count = self.get('max_display_page_count'),
+                a = '', i, start, end, first_page = self.get('first_page'), end_page = self.get('total_page');
+
+            a += '<a href="#" class="' + cls + 'prev ' + (current_page <= first_page ? cls + 'disabled' : '') + '">' + self.get('prev_text') + '</a>';
+            if (count) {
+                end_page -= 1;
+                start = Math.max(first_page, parseInt(current_page - count / 2));
+                end = Math.min(end_page, start + count);
+
+                if (self.get('ellipse_text') && start > first_page) {
+                    a += '<span class="' + cls + 'item">...</span>';
+                }
+
+                for (i = start; i <= end; i++) {
+                    if (i !== current_page) {
+                        a += '<a href="#" class="' + cls + 'page ' + cls + 'item">' + (i + 1) + '</a>';
+                    } else {
+                        a += '<span class="' + cls + 'current ' + cls + 'item">' + (i + 1) + '</span>';
+                    }
+                }
+                if (self.get('ellipse_text') && end < end_page) {
+                    a += '<span class="' + cls + 'item">...</span>';
+                }
+
+                // 判断是否具有下一页
+                if (current_page < end_page) self.set('has_next', true);
+            }
+
+            a += '<a href="#" class="' + cls + 'next ' + (!self.get('has_next') ? cls + 'disabled' : '') + '">' + self.get('next_text') + '</a>';
+
+            self.get('container').html(a);
+        },
+        /**
+         * 绑定点击事件, 做页面切换
+         */
+        _bind: function() {
+            var self = this,
+                cls = self.get('cls');
+
+            self.get('container').on('click', function(e) {
+                var target = new S.Node(e.target), idx = self.get('current_page');
+                if (target.hasClass(cls + 'disabled') || self.__loading) {
+                    e.preventDefault();
+                    return;
+                } else if (target.hasClass(cls + 'page')) {
+                    idx = parseInt(target.html()) - 1;
+                } else if (target.hasClass(cls + 'prev')) {
+                    idx -= 1;
+                } else if (target.hasClass(cls + 'next')) {
+                    idx += 1;
+                } else {
+                    return;
+                }
+                if (!self.page(idx)) e.preventDefault();
+            });
+        },
+        page: function(idx) {
+            var self = this,
+                cb = self.get('callback');
+            self.__loading = true;
+
+            // 加载完某页后续工作
+            function ready(idx) {
+                self.set('current_page', idx);
+                self.update();
+                self.__loading = false;
+            }
+            return cb(idx, self, ready);
+
+        }
+    });
+
+	//私有方法
+
+    //兼容 1.1.6
+    S.namespace('Gallery');
+    S.Gallery.Pagenation = Pagenation;
+
+    return Pagenation;
+}, {
+    requires: ["core"]
+});
