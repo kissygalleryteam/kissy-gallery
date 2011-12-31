819a435324c88810b2c8c22e0dc94943f1d85c5b
liz <shengyan1985@gmail.com>
2011-12-31 15:39:05 +0800

    add selectable

---
+++ gallery/selectable/1.0/index.js
@@ -0,0 +1,8 @@
+KISSY.add("gallery/selectable/1.0/index", function(S,Selectable, ComboBox){
+    return {
+        Selectable: Selectable,
+        ComboBox: ComboBox
+    };
+},{
+    requires:["./base", "./combobox"]
+});
