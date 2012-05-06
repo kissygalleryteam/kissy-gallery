eb15e777a4a045df28dca7765aa3373aa88823b8
minghe <minghe36@126.com>
2012-05-06 17:44:56 +0800

    fixed #51

--- gallery/form/1.2/uploader/themes/default/index.js
+++ gallery/form/1.2/uploader/themes/default/index.js
@@ -52,18 +52,26 @@ KISSY.add('gallery/form/1.2/uploader/themes/default/index', function (S, Node, T
                  queue = self.get('queue'),
                  //上传方式
                  uploadType = uploader.get('type'),
+                 //进度条容器
                  $progressBar = $('.J_ProgressBar_' + ev.id);
             //如果是ajax或flash异步上传，加入进度条
             if(uploadType == 'ajax' || uploadType == 'flash'){
                 var ProgressBar = self.get('oPlugin').progressBar,progressBar;
                 if(ProgressBar){
                     progressBar = new ProgressBar($progressBar);
+                    progressBar.on('change',function(ev){
+                        //百分百进度隐藏进度条
+                        if(ev.value == 100){
+                            progressBar.hide();
+                        }
+                    });
                     progressBar.render();
-                    self.set('progressBar',progressBar);
+
                 }
                 //将进度条实例写入到队列的文件数据上备用
                 queue.updateFile(index,{progressBar:progressBar});
             }
+
         },
         /**
          * 文件处于正在上传状态时触发
@@ -84,7 +92,16 @@ KISSY.add('gallery/form/1.2/uploader/themes/default/index', function (S, Node, T
          * 文件处于上传成功状态时触发
          */
         _successHandler:function(ev){
-
+            var file = ev.file,
+                id = file.id,
+                $del = $(".J_Del_" + id),
+                $cancel = $(".J_Cancel_" + id),
+                progressBar = file.progressBar;
+            $del.show();
+            $cancel.hide();
+            if(!progressBar) return false;
+            //处理进度
+            progressBar.set('value',100);
         },
         /**
          * 文件处于上传错误状态时触发
@@ -119,11 +136,11 @@ KISSY.add('gallery/form/1.2/uploader/themes/default/index', function (S, Node, T
                 '<div class="f-l">{name}</div>' +
                 '<div class="f-l status-wrapper J_FileStatus">' +
                     '<div class="status waiting-status">等待上传，<a class="J_Upload_{id}" href="#Upload">点此上传</a> </div>' +
-                    '<div class="status start-status progress-status clearfix">' +
+                    '<div class="status start-status progress-status success-status clearfix">' +
                         '<div class="J_ProgressBar_{id} f-l uploader-progress"><img class="loading" src="http://img01.taobaocdn.com/tps/i1/T1F5tVXjRfXXXXXXXX-16-16.gif" alt="loading" /></div>' +
                         ' <a  class="J_Cancel_{id} f-l upload-cancel" href="#uploadCancel">取消</a>' +
+                        '<a href="#fileDel" class="J_Del_{id}" style="display:none;">删除</a>' +
                     '</div> ' +
-                    ' <div class="status success-status"><a href="#fileDel" class="J_Del_{id}">删除</a></div>' +
                     '<div class="status cancel-status">已经取消上传，<a href="#reUpload" id="J_ReUpload_{id}" class="J_Upload_{id}">点此重新上传</a> </div>' +
                     '<div class="status error-status upload-error"><span class="J_ErrorMsg_{id}"></span><a href="#fileDel" class="J_Del_{id}">删除</a></div>' +
                 '</div>' +
