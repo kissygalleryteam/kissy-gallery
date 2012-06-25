f55a77dcb00a0cc5b4ceec59ce6d579360e1117b
minghe <minghe36@126.com>
2012-06-25 17:48:56 +0800

    v1.2.6 uploader demo

---
+++ gallery/form/1.2/demo/assets/app.js
@@ -0,0 +1,27 @@
+!function ($) {
+
+    $(function(){
+        // fix sub nav on scroll
+        var $win = $(window)
+            , $nav = $('.subnav')
+            , navHeight = $('.navbar').first().height()
+            , navTop = $('.subnav').length && $('.subnav').offset().top - navHeight
+            , isFixed = 0
+
+        processScroll()
+
+        $win.on('scroll', processScroll)
+
+        function processScroll() {
+            var i, scrollTop = $win.scrollTop()
+            if (scrollTop >= navTop && !isFixed) {
+                isFixed = 1
+                $nav.addClass('subnav-fixed')
+            } else if (scrollTop <= navTop && isFixed) {
+                isFixed = 0
+                $nav.removeClass('subnav-fixed')
+            }
+        }
+    })
+
+}(window.jQuery)
