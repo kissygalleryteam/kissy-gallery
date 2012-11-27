438822333a2509588e471d2722a155537e90a082
yiminghe <yiminghe@gmail.com>
2012-11-28 01:46:13 +0800

    add input-cursor

--- index.js
+++ index.js
@@ -2,176 +2,181 @@
    "gs":[
    {
         "name":"velocity",
-        
+
         "desc":"velocity in js"
+    },
+    {
+      "name":"input-cursor",
+
+       "desc":"取得输入框光标位置"
     },
    {
         "name":"image-dd",
-        
+
         "desc":"图片拖拽缩放"
     },
     {
         "name":"grid",
-        
+
         "desc":"表格 grid"
     },
     {
         "name":"chart",
-        
+
         "desc":"Kissy 图表引擎，提供条形图，折线图基于HTML5 Canvas， 支持IE"
     },
     {
         "name":"countdown",
-        
+
         "desc":"倒计时组件"
     },
     {
         "name":"timeline",
-        
+
         "desc":"时间轴 timeline"
     },
-    {
-        "name":"randombox",
-        
-        "desc":"转盘摇奖"
-    },
-    {
-        "name":"checkcode",
-        
-        "desc":"验证码"
-    },
-    
+
     {
         "name":"form",
-        
+
         "desc":"表单组件集合（异步文件上传）"
     },
     {
         "name":"kscroll",
-        
+
         "desc":"滚动条组件"
     },
     {
+        "name":"randombox",
+
+        "desc":"转盘摇奖"
+    },
+    {
         "name":"scrollspy",
-        
+
         "desc":"scrollspy"
     },
     {
         "name":"magnifier",
-        
+
         "desc":"圆形放大器"
-    },    
+    },
+    {
+        "name":"checkcode",
+
+        "desc":"验证码"
+    },
     {
         "name":"spotlight",
-        
+
         "desc":"聚焦灯效果"
     },
     {
         "name":"huabao",
-        
+
         "desc":"画报组件"
     },
     {
         "name":"urlparser",
-        
+
         "desc":"urlparser"
     },
     {
         "name":"wwgroup",
-        
+
         "desc":"ww 点灯"
     },
     {
         "name":"juicer",
-        
+
         "desc":"最快的模板引擎"
     },
     {
         "name":"sliding-labels",
-        
+
         "desc":"表单label浮动功能"
     },
     {
         "name":"digital-clock",
-        
+
         "desc":"数字时钟"
-    },    
-    
+    },
+
     {
         "name":"easycountdown",
-        
+
         "desc":"倒计时组件，不同于传统倒计时组件的实现方式，容易扩展"
     },
     {
         "name":"expose",
-        
+
         "desc":"开关灯，同优酷视频播放的黑白屏效果"
     },
-    
+
     {
         "name":"couple-select",
-        
+
         "desc":"模拟MultiSelect的功能，能排除已选择的项，有单熏全选的功能"
     },
     {
         "name":"loading",
-        
+
         "desc":"显示/关闭加载状态"
     },
     {
         "name":"random-show",
-        
+
         "desc":"随机显示效果"
     },
     {
         "name":"reflection",
-        
+
         "desc":"图片倒影效果"
     },
     {
         "name":"pagination",
-        
+
         "desc":"分页"
     },
     {
         "name":"starrating",
-        
+
         "desc":"打分"
     },
     {
         "name":"local-storage",
-        
+
         "desc":"本地存储"
     },
     {
         "name":"browsercache",
-        
+
         "desc":"浏览器端的缓存机制"
     },
     {
         "name":"mustache",
-        
+
         "desc":"mustache 模板引擎"
     },
     {
         "name":"image-tagging",
-        
+
         "desc":"图片标记组件"
     },
     {
         "name":"province",
-        
+
         "desc":"省市区联动组件，淘宝最新省市区数据"
     },
     {
         "name":"combine",
-        
+
         "desc":"适用 KISSY 1.2 的自动 combo patch"
     },
     {
         "name":"selectable",
-        
+
         "desc":"选择/下拉框"
     }
-   ]   
+   ]
 }
