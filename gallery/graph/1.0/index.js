2f178f0f447d8478eb60b36dd294311f2eb707f2
feichang <pswho@yahoo.cn>
2013-01-06 13:16:17 +0800

    添加kissy graph组件

---
+++ gallery/graph/1.0/index.js
@@ -0,0 +1,17 @@
+/**
+ * KISSY Graph
+ * 基于SVG VML的图表组件 支持ie6+ chrome firefox safari opera .eg
+ * @author 飞长<veryued@gmail.com>
+ */
+
+KISSY.add('gallery/graph/1.0/index', function(S, SumDetail){
+    var $ = Node.all;
+    function Graph(){}
+    
+    Graph.SumDetail = SumDetail;
+
+    return Graph;
+
+}, {
+    requires: ['./sumDetail']
+});
